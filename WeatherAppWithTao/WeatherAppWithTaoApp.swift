1139806ee517e3dad8d306d6e9ad18739d841a28
Alexey Kupriyanov <mr.akupr@gmail.com>
2026-02-16 00:16:39 +0300

    Initial Commit

---
+++ WeatherAppWithTao/WeatherAppWithTaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppWithTaoApp.swift
+//  WeatherAppWithTao
+//
+//  Created by Alexey Kupriyanov on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppWithTaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppWithTaoTests/WeatherAppWithTaoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppWithTaoTests.swift
+//  WeatherAppWithTaoTests
+//
+//  Created by Alexey Kupriyanov on 16.02.2026.
+//
+
+import Testing
+@testable import WeatherAppWithTao
+
+struct WeatherAppWithTaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
